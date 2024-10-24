6dcbc8cea839ecd4f5bae68c3ab56183f4fb8151
G08052008 <s.polyna08@gmail.com>
2024-10-24 15:23:41 +0300

    да

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+    const name = "Спирина Полина Андреевна";
+    const element = document.querySelector("#name");
+    if (element)
+    {
+        element.textContent = name;
+    }
+}
